17d45a52dd56bc4fae54e0d8bf340b7f0404a341
vivek166 <vivek.tiwari.cse16@gmail.com>
2018-01-26 15:23:48 +0530

    first commit

---
+++ api/delete.php
@@ -0,0 +1,7 @@
+require '../db_config.php';
+
+ $id  = $_GET["id"];
+ $sql = "DELETE FROM items WHERE id = '".$id."'";
+ $result = $mysqli->query($sql);
+
+echo json_encode([$id]);
---
+++ api/edit.php
@@ -0,0 +1,8 @@
+require '../db_config.php';
+
+  $id  = $_GET["id"];
+  $sql = "SELECT * FROM items WHERE id = '".$id."'"; 
+  $result = $mysqli->query($sql);
+  $data = $result->fetch_assoc();
+
+echo json_encode($data);
